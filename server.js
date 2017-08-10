7fdb2fc9af713c09bad78e84eaf4c35b8e30cb86
Dory <dorybaga@gmail.com>
2017-08-09 21:54:48 -1000

    server is running

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 /*jshint esversion: 6*/
 
+const express = require('express');
+const exphbs = require('express-handlebars');
+const router = express.Router();
+const bp = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const db = require('./models');
+
+const app = express();
+
+
+const server = app.listen(PORT, () => {
+  db.sequelize.sync();
+  console.log(`server running on ${PORT}`);
+});
