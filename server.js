00532842bad1c9f18ee823edbc9114ef51b8fa11
Dory <dorybaga@gmail.com>
2017-08-10 21:27:01 -1000

    added method-overide

--- server.js
+++ server.js
@@ -17,27 +17,53 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.use(bp.urlencoded());
 
-app.get('/', (req, res) => {
-  console.log('show gallery of imgs');
-  res.render('/'); // <---- takes obj as 2nd arg, { what goes here??? }
 
+router.route('/gallery')
+  .get((req, res) => {
+  console.log('GET req recieved');
+  Gallery.findAll()
+  .then((gallery) => {
+    console.log("this is the gallery: ", gallery);
+    res.render('index', {gallery: gallery});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+})
+  .post((req, res) => {
+    Gallery.create({
+      author: req.body.author,
+      link: req.body.link,
+      description: req.body.description
+  })
+  .then((data) => {
+    console.log(data);
+    console.log('New image added to gallery!');
+    res.end();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 });
 
-router.route('/gallery/:id')
-  // should render img with specified id ** GET
-  .get((req, res) => {
+// router.route('/gallery/:id')
+//   // should render img with specified id ** GET
+//   .get((req, res) => {
 
-  })
+//   })
 
-  // should have link to edit ** PUT
-  .put((req, res) => {
+//   // should have link to edit ** PUT
+//   .put((req, res) => {
 
-  })
+//   })
 
-  // should have link to delete ** DELETE
-  .delete((req, res) => {
+//   // should have link to delete ** DELETE
+//   .delete((req, res) => {
 
-  });
+//   });
+
+
+
 
 const server = app.listen(PORT, () => {
   db.sequelize.sync();
