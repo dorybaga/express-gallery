232ff86b119f4bd91434e530f47fbe02f2ded201
Dory <dorybaga@gmail.com>
2017-08-12 16:07:36 -1000

    added temp psql file to insert values to Galleries

---
+++ imgdata.sql
@@ -0,0 +1,25 @@
+\c express_gallery
+
+INSERT INTO "Galleries" VALUES
+(default,
+'Craig Whitehead',
+'https://images.unsplash.com/photo-1494018405518-a1c52e534bad?dpr=1&auto=format&fit=crop&w=1500&h=1875&q=80&cs=tinysrgb&crop=',
+'Kings Parade, Cambridge UK',
+NOW(),
+NOW());
+
+INSERT INTO "Galleries" VALUES
+(default,
+'Pepe Nero',
+'https://images.unsplash.com/photo-1457630509638-7a7543f31f75?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=',
+'Metropolitan City of Rome, Italy',
+NOW(),
+NOW());
+
+INSERT INTO "Galleries" VALUES
+(default,
+'Anthony Menecola',
+'https://images.unsplash.com/photo-1482864521648-c7bd458ce921?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=',
+'Walt Disney Concert Hall, Los Angeles, US',
+NOW(),
+NOW());
