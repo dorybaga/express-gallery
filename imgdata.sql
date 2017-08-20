107fc3e33a40ab66f74a0bae2a3fc4fa470a204f
Dory <dorybaga@gmail.com>
2017-08-19 19:42:45 -1000

    added more data to DB

--- imgdata.sql
+++ imgdata.sql
@@ -23,3 +23,38 @@ INSERT INTO "Galleries" VALUES
 'Walt Disney Concert Hall, Los Angeles, US',
 NOW(),
 NOW());
+
+
+INSERT INTO "Galleries" VALUES
+(default,
+'Biasol',
+'/877985/gwynne-st-studio-biasol/5996d3eab22e3862a8000103-gwynne-st-studio-biasol-photo',
+'Gwynne St, Australia',
+NOW(),
+NOW());
+
+
+INSERT INTO "Galleries" VALUES
+(default,
+'WSDIA',
+'http://images.adsttc.com/media/images/5992/a93f/b22e/38d0/4800/0848/newsletter/15_4th-floor_showrooms.jpg?1502783798',
+'Nike New York Headquarters',
+NOW(),
+NOW());
+
+INSERT INTO "Galleries" VALUES
+(default,
+'Unknown',
+'http://interiorsbystudiom.com/wp-content/uploads/2016/03/4-15.jpg',
+'Adaptive Reuse - United States',
+NOW(),
+NOW());
+
+
+INSERT INTO "Galleries" VALUES
+(default,
+'Louis Kahn',
+'http://www.trbimg.com/img-5834d5e0/turbine/la-et-cam-salk-institute-louis-kahn-20161107',
+'Salk Institute, San Diego, CA',
+NOW(),
+NOW());
